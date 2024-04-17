b0b4c09359e1af40d62a7d8efe9f799276b0788e
idknow <joyoona4174@gmail.com>
2024-04-17 21:22:50 +0900

    api/pill/search (투여기간주의 완료)

--- src/controllers/python/searchPill.py
+++ src/controllers/python/searchPill.py
@@ -62,7 +62,62 @@ def searchPill(pillSeq):
         else:
             result2 = "용량 주의 사항 없음"
 
-        result = result1 + "\n" + result2
+        #투여기간주의
+        result_data_per = list()
+        csv = pd.read_csv('C:/Users/82104/Node_lecture/medication-alarm/src/controllers/python/투여기간.csv', names=['kor_name','eng_name','type','max_period','alpha'])
+        df = pd.DataFrame(csv)
+        url = "http://apis.data.go.kr/1471000/DURPrdlstInfoService03/getMdctnPdAtentInfoList03?serviceKey=NDY4F%2BZ3KM7why4fZ7kxeMoMsKUPDZULW3AGi2MBsbbCDbftlWdqpWspCKRgh8%2B3X802AreQijV0rQeD4seuFw%3D%3D&pageNo=1&numOfRows=3&type=xml&typeName=%ED%88%AC%EC%97%AC%EA%B8%B0%EA%B0%84%EC%A3%BC%EC%9D%98&ingrCode=D001370&start_change_date=20190705&itemSeq=" + str(pillSeq)
+        response = requests.get(url, verify=False)
+        contents = response.text
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        count = soup.findAll('totalcount')
+        if count:
+            totalcount = int(count[0].text.strip())
+        else:
+            totalcount = 0
+        itemList = soup.findAll('item')
+
+        if(totalcount > 0):
+            for item in itemList:
+                name = item.find('item_name').text
+                seq = item.find('item_seq').text
+                ingr = item.find('ingr_name').text
+                mixtype = item.find('mix_type').text
+                mixIngr = item.find('mix_ingr').text
+
+            if mixtype == '단일':
+                contains_str = df['kor_name'].str.contains(ingr)
+                if contains_str.any():
+                    index_where_contains = df[~df['alpha'].astype(str).str.contains('복합제') & contains_str].index.tolist()
+                    idx = index_where_contains[0]
+                    maxPeriod = df.loc[idx, 'max_period']
+                    result3 = "투여기간 최대 " + maxPeriod
+            
+            else:
+                temp = re.sub(r'[^ㄱ-ㅎ가-힣]+', '/', mixIngr)
+                result = temp.split('/')
+                result = list(filter(None, result))
+                contains_all = None
+
+                for ingr in result:
+                    contains_ingr = df['kor_name'].str.contains(ingr)
+                    if contains_all is None:
+                        contains_all = contains_ingr
+                    else:
+                        contains_all &= contains_ingr
+    
+                if contains_all.any():
+                    index_where_contains_all = df[contains_all].index.tolist()
+                    for idx in index_where_contains_all:
+                        maxPeriod = df.loc[idx, 'max_period']
+                        result3 = "투여기간 최대 " + maxPeriod
+                else:
+                    result3 = "투여기간 주의 사항 찾을 수 없음"
+        else:
+            result3 = "투여기간 주의 사항 없음"
+
+        result = result1 + "\n" + result2 + "\n" + result3
 
     except requests.exceptions.RequestException as e:
         result = f"오류 발생: {e}"
